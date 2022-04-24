72a5de644e56ef39de5ad9c5bf5d4d21964f000e
wuhe <wuhe@tmzy11.wecom.work>
2022-04-24 19:00:41 +0800

    fix: 发包问题修复

--- packages/index.ts
+++ packages/index.ts
@@ -4,13 +4,13 @@ import CanvasContainer from "./components/CanvasContainer/index";
 import BarGraph from "./components/BarGraph/index";
 import LineGraph from "./components/LineGraph/index";
 import PieGraph from "./components/PieGraph/index";
-
-const componentList = [BarGraph, LineGraph, PieGraph];
+import SunburstGraph from "./components/SunburstGraph/index";
+const componentList = [BarGraph, LineGraph, PieGraph,SunburstGraph];
 
 const install = (app: App): void => {
   componentList.forEach((component) => {
     app.component(component.name, component);
   });
 };
-export { BarGraph, LineGraph, PieGraph };
+export { BarGraph, LineGraph, PieGraph,SunburstGraph };
 export default install;
